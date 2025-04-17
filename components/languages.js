ae4d7197c27181d5d7268d8e738dd692a9a2b746
Leanstix <leanstixx@gmail.com>
2025-04-17 11:59:45 +0100

    set

--- components/languages.js
+++ components/languages.js
@@ -1,3 +1,4 @@
+// /pages/api/languages.js
 export default async function handler(req, res) {
     const { url } = req.query;
   
@@ -6,13 +7,23 @@ export default async function handler(req, res) {
     try {
       const githubRes = await fetch(url, {
         headers: {
-          Authorization: `token ${process.env.GITHUB_PAT}`,
+          Authorization: `token ${process.env.GITHUB_PAT}`, // Ensure this is being passed correctly
           Accept: "application/vnd.github+json",
         },
       });
+  
+      if (githubRes.status === 403) {
+        return res.status(403).json({ error: "Forbidden: Check GitHub Token permissions or rate limit" });
+      }
+  
       const data = await githubRes.json();
-      res.status(200).json(data);
+      if (githubRes.ok) {
+        res.status(200).json(data);
+      } else {
+        res.status(500).json({ error: data.message || "Failed to fetch repo languages" });
+      }
     } catch (error) {
+      console.error("Error:", error);
       res.status(500).json({ error: "Failed to fetch repo languages" });
     }
   }
