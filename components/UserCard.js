2436b4589cd010e66c77d006dece933621f63bc0
Leanstix <leanstixx@gmail.com>
2025-04-17 11:02:32 +0100

    working

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
 export default function UserCard({ user }) {
     return (
       <div className="max-w-xl mx-auto bg-white p-4 rounded shadow mb-6">
         <div className="flex items-center gap-4">
-          <Image src={user.avatar_url} alt="avatar" className="w-16 h-16 rounded-full" />
+          <img src={user.avatar_url} alt="avatar" className="w-16 h-16 rounded-full" />
           <div>
             <h2 className="text-xl font-semibold">{user.name || user.login}</h2>
             <p className="text-gray-600">{user.bio}</p>
