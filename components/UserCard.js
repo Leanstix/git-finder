29b2a5ba982f6ab77c50855bb758dc8c7eba51f7
Leanstix <leanstixx@gmail.com>
2025-04-17 12:14:55 +0100

    set

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,18 +1,12 @@
 // components/UserCard.js
 export default function UserCard({ user }) {
-    if (!user) return null;
-  
     return (
       <div className="max-w-xl mx-auto bg-white p-4 rounded shadow mb-6">
         <div className="flex items-center gap-4">
           <img src={user.avatar_url} alt="avatar" className="w-16 h-16 rounded-full" />
           <div>
             <h2 className="text-xl font-semibold">{user.name || user.login}</h2>
-            {user.bio ? (
-              <p className="text-gray-600">{user.bio}</p>
-            ) : (
-              <p className="text-gray-400 italic">No bio available</p>
-            )}
+            <p className="text-gray-600">{user.bio}</p>
           </div>
         </div>
       </div>
